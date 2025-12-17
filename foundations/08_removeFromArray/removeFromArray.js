434e096ff66fdf6a5fe1a980e07165d97130978e
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2025-12-17 12:39:32 -0800

    solved problem #8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = (arr, ...removeVal) => {
+  return arr.filter((item) => !removeVal.includes(item));
 };
 
 // Do not edit below this line
