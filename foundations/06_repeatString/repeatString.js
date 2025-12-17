40c1a54940f1b29ebf367f550560a7d42addefd5
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2025-12-17 12:06:09 -0800

    solved solution 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  let combineWords = "";
+  for (let i = 0; i < num; i++) {
+    combineWords = combineWords.concat(string);
+  }
+  return combineWords;
 };
 
 // Do not edit below this line
