7177ac8fba0a287e5846bcdbd1e6ba5e0dea06d9
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2025-12-17 17:01:29 -0800

    solved problem #11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = (temperature) => {
+  const tempC = ((temperature - 32) * 5) / 9;
+  return Math.round(tempC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (temperature) => {
+  const tempF = (temperature * 9) / 5 + 32;
+  return Math.round(tempF * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
